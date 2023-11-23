d389ede77cdd9861a08d935d9a615ff3a497ef2e
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2023-11-23 18:08:24 +0100

    inseriti orari ed aggiunto l'intervallo e la chiamata per attivarla

--- js/script.js
+++ js/script.js
@@ -5,5 +5,20 @@ function capodanno() {
     //data per capodanno
     let capodanno = new Date(oggi.getFullYear() + 1, 0, 1); 
 
-    
-}
+    let differenza = capodanno - oggi; 
+    let secondi = Math.floor(differenza / 1000) % 60;
+    let minuti = Math.floor(differenza / 1000 / 60) % 60;
+    let ore = Math.floor(differenza / 1000 / 60 / 60) % 24;
+    let giorni = Math.floor(differenza / 1000 / 60 / 60 / 24);
+       // Visualizzazione del countdown
+       let countdownASchermo = document.getElementById('countdown');
+       countdownASchermo.innerHTML = giorni + " giorni, " + ore + " ore, " + minuti + " minuti, " + secondi + " secondi";
+}    
+   
+// Aggiornamento del countdown per ogni secondo
+setInterval(capodanno, 1000);
+   
+     
+capodanno();
+   
+
